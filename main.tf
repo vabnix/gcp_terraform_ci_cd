208871d6a39a5f94a74d1630fa974b433898b67d
Vaibhav Srivastava <vabnix@hotmail.com>
2024-09-10 21:55:50 -0500

    updating provider and file

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "aum_bucket" {
   name                     = "aum-github-bucket"
+  project                  = "vaibhav-gcp-01-433314-t5"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
