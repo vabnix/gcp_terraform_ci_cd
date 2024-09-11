5b03d420237ce2e4bcb731213deb4bd700f33cad
Vaibhav Srivastava <vabnix@hotmail.com>
2024-09-10 22:04:34 -0500

    adding infra

--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
-resource "google_storage_bucket" "aum_bucket" {
-  name                     = "aum-github-bucket"
-  project                  = "vaibhav-gcp-01-433314-t5"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+resource "google_compute_network" "vpc_network" {
+  name                    = var.network
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  name          = var.subnetwork
+  ip_cidr_range = "10.20.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = var.os_image
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.vpc_subnetwork.id
+    access_config {
+    }
+  }
 }
